6210df66754525b97a1d1ac2888c366667ecfe06
F_picacho <2314008827@qq.com>
2022-07-18 16:51:56 +0800

    feat(packages/reactivity/src/index.ts): 定义reactive函数
    初步实现了将函数包装为代理对象逻辑

--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -1,3 +1,19 @@
 import {isObject} from '@vue/shared'
 
-console.log(isObject([]))
+// reactive:将数据转换为响应式
+export function reactive(target) {
+    if (!isObject(target)) {
+        throw new TypeError(`目标${target}类型不为对象!`)
+    }
+    /*  new Proxy(代理目标,代理行为)
+        https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Proxy */
+    return new Proxy(target, {
+        get(target, key, receiver) {
+            return target[key]
+        },
+        set(target, key, value, receiver) {
+            target[key] = value
+            return true
+        }
+    })
+}
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,4 @@
+// isObject:判断value是否为对象
 export const isObject = (value) => {
     return typeof value === 'object' && value !== null
 }
