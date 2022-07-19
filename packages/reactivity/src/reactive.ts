8099ad53e5e9111f8df276d0767bf9221fa32363
F_picacho <2314008827@qq.com>
2022-07-19 11:47:41 +0800

    style(./*): 更新代码风格

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,25 +1,24 @@
-import {isObject} from '@vue/shared'
-import {mutableHandlers,ReactiveFlags} from './baseHandler'
+import { isObject } from "@vue/shared";
+import { mutableHandlers, ReactiveFlags } from "./baseHandler";
 
 const reactiveMap = new WeakMap(); // 弱引用版本的Map Key只能是对象 用于解决潜在的内存泄露问题
 
 export function reactive(target) {
-    // reactive:将数据转换为响应式
-    if (!isObject(target)) {
-        throw new TypeError(`目标${target}类型不为object!`)
-    }
-    // 判断target的是否为另外一个代理对象
-    if (target[ReactiveFlags.IS_REACTIVE]) {
-        return target
-    }
+  // reactive:将数据转换为响应式
+  if (!isObject(target)) {
+    throw new TypeError(`目标${target}类型不为object!`);
+  }
+  // 判断target的是否为另外一个代理对象
+  if (target[ReactiveFlags.IS_REACTIVE]) {
+    return target;
+  }
+  // 利用reactiveMap进行缓存 防止一个对象
+  let existingProxy = reactiveMap.get(target);
+  if (existingProxy) {
+    return existingProxy;
+  }
 
-    // 利用reactiveMap进行缓存 防止一个对象
-    let existingProxy = reactiveMap.get(target)
-    if (existingProxy) {
-        return existingProxy
-    }
-
-    const proxy = new Proxy(target, mutableHandlers)
-    reactiveMap.set(target, proxy)
-    return proxy
+  const proxy = new Proxy(target, mutableHandlers);
+  reactiveMap.set(target, proxy);
+  return proxy;
 }
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,4 @@
 // isObject:判断value是否为对象
 export const isObject = (value) => {
-    return typeof value === 'object' && value !== null
-}
+  return typeof value === "object" && value !== null;
+};
